23f4d65ae23bc8f7d080b28d602267fb7e990343
Pareekshithmk <pareekshithnvn@gmail.com>
2026-08-31 22:32:50 +0530

    Initialize NexOps Spring Boot backend

---
+++ src/main/java/com/nexops/NexopsApplication.java
@@ -0,0 +1,11 @@
+package com.nexops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexopsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NexopsApplication.class, args);
+    }
+}
---
+++ src/test/java/com/nexops/NexopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
